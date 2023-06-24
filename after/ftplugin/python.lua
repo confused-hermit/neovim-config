17b205107d56761798d33c885972a1cf91f77cb5
hermit-confused <hermit.confused@gmail.com>
2023-06-24 18:32:20 +0530

    added python ftplugin

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,1 +1,1 @@
-vim.keymap.set("n", "<leader>la", ":PyrightOrganizeImports<cr>", "organize imports")
+vim.keymap.set("n", "<leader>la", ":PyrightOrganizeImports<cr>", {desc = "organize imports", silent=true})
